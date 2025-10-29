767252e0142b836692facd69098a3ef9e778ce74
RCNR <iop04226@gmail.com>
2025-10-29 14:57:06 +0900

    initial commit

---
+++ src/main/java/com/example/orderjobabom/OrderJobAbomApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderjobabom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderJobAbomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderJobAbomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orderjobabom/OrderJobAbomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderjobabom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderJobAbomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
