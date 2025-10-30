1e98f1da2b4512e971dbf0aa8cd927cf1608f87a
RCNR <iop04226@gmail.com>
2025-10-30 10:17:51 +0900

    feat: ExceptionHandlerAdvice 구현

--- src/main/java/com/example/orderjobabom/global/presentation/error/GeneralErrorCode.java
+++ src/main/java/com/example/orderjobabom/global/presentation/error/GeneralErrorCode.java
@@ -12,7 +12,7 @@ public enum GeneralErrorCode implements BaseErrorCode {
     UNAUTHORIZED_401(HttpStatus.UNAUTHORIZED, "401", "인증이 필요합니다."),
     FORBIDDEN_403(HttpStatus.FORBIDDEN, "403", "권한이 없는 잘못된 접근입니다."),
     NOT_FOUND_404(HttpStatus.BAD_REQUEST, "404", "요청 자원을 찾을 수 없습니다."),
-    NOT_FOUND_500(HttpStatus.INTERNAL_SERVER_ERROR, "500", "서버 내부 오류입니다.");
+    INTERNAL_SERVER_500(HttpStatus.INTERNAL_SERVER_ERROR, "500", "서버 내부 오류입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/com/example/orderjobabom/global/presentation/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/example/orderjobabom/global/presentation/exception/ExceptionHandlerAdvice.java
@@ -2,31 +2,76 @@ package com.example.orderjobabom.global.presentation.exception;
 
 import com.example.orderjobabom.global.presentation.CustomResponse;
 import com.example.orderjobabom.global.presentation.error.BaseErrorCode;
+import com.example.orderjobabom.global.presentation.error.ErrorReasonDTO;
+import com.example.orderjobabom.global.presentation.error.GeneralErrorCode;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice(annotations = RestControllerAdvice.class)
 @Slf4j
 public class ExceptionHandlerAdvice {
 
-    // 커스텀 예외
-    @ExceptionHandler(FailException.class)
-    public ResponseEntity<CustomResponse<?>> handleCustomException(FailException e) {
+    // 일반 예외
+    @ExceptionHandler(GeneralException.class)
+    public ResponseEntity<CustomResponse<?>> handleGeneralException(GeneralException e) {
 
-        BaseErrorCode errorCode = e.getErrorCode();
-        CustomResponse<?> response =
+        BaseErrorCode baseErrorCode = e.getBaseErrorCode();
+        ErrorReasonDTO dto = baseErrorCode.getReasonHttpStatus();
 
-        return new ResponseEntity<>()
+        log.error("General Exception: [{}]", dto.getMessage());
 
+        return ResponseEntity.status(dto.getHttpStatus()).body(CustomResponse.onFailure(dto.getMessage(), dto.getCode(), null));
     }
 
     // 일반 예외
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<CustomResponse<?>> handleException(Exception e) {}
+    public ResponseEntity<CustomResponse<?>> handleException(Exception e) {
 
-    // DTO 검증 처리
+        ErrorReasonDTO dto = GeneralErrorCode.INTERNAL_SERVER_500.getReasonHttpStatus();
+        log.error("Exception : [{}] ", e.getClass().getSimpleName(), e.getMessage());
 
+        String failMessage = e.getClass().getSimpleName() + " " + e.getMessage();
+
+        return ResponseEntity.status(dto.getHttpStatus()).body(CustomResponse.onFailure(dto.getMessage(), dto.getCode(), failMessage));
+    }
+
+    // 컨트롤러 파라미터 유효성 실패
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomResponse<Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        Map<String, String> errorMap = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach((fieldError) -> {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+
+        ErrorReasonDTO dto = GeneralErrorCode.BAD_REQUEST_400.getReasonHttpStatus();
+        log.error("MethodArgumentNotValidException: [{}]", errorMap);
+
+        return ResponseEntity.status(dto.getHttpStatus()).body(CustomResponse.onFailure(dto.getMessage(), dto.getCode(), errorMap));
+    }
+
+    // 유효성 검사 실패
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<CustomResponse<List<String>>> constraintViolationException(ConstraintViolationException e) {
+
+        log.error(Arrays.toString(e.getStackTrace()));
+
+        List<String> errorMessages = e.getConstraintViolations().stream().map(constraintViolation -> constraintViolation.getMessage()).toList();
+
+        ErrorReasonDTO dto = GeneralErrorCode.BAD_REQUEST_400.getReasonHttpStatus();
+        log.error("ConstraintViolationException: [{}]", dto.getMessage());
+
+        return ResponseEntity.status(dto.getHttpStatus()).body(CustomResponse.onFailure(dto.getMessage(), dto.getCode(), errorMessages));
+
+    }
 
 }
