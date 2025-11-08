f2ecb3e60a1afbfbf5fad2f37b7a9a60c65280e9
RCNR <iop04226@gmail.com>
2025-11-08 13:03:42 +0900

    refactor: 이벤트 설정

--- src/main/java/com/example/orderjobabom/global/infrastructure/event/Events.java
+++ src/main/java/com/example/orderjobabom/global/infrastructure/event/Events.java
@@ -1,11 +1,11 @@
-package com.example.orderjobabom.global.presentation.event;
+package com.example.orderjobabom.global.infrastructure.event;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class Events {
 
@@ -18,6 +18,15 @@ public class Events {
     }
 
     public static void raise(Object event) {
+        staticPublisher.publishEvent(event);
+    }
+
+    public static void setPublisher(ApplicationEventPublisher publisher) {
+        Events.staticPublisher = publisher;
+    }
+
+    public static void trigger(Object event) {
+        if (staticPublisher == null) return;
         staticPublisher.publishEvent(event);
     }
 }
